0d3f02ecb14a9398dbb5ab533d61f122d5c9fdf5
Easton Morris <eastonmorris9@gmail.com>
2021-05-15 14:15:32 -0700

    added the photo preview and prevDef script

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 /* global data */
 /* exported data */
+
+var $photo = document.querySelector('.placeholder-img');
+var $urlField = document.querySelector('#photo-url');
+
+document.addEventListener('submit', function (event) {
+  event.preventDefault();
+});
+
+document.addEventListener('input', function (event) {
+  $photo.setAttribute('src', $urlField.value);
+});
