85d75ca55dbe142e0a7734a5f581d2b3fa533376
Easton Morris <eastonmorris9@gmail.com>
2021-05-15 16:07:28 -0700

    fixed centering and sizing on img, fixed incrementing on localstorage, removed unused classes and added a new one

--- js/data.js
+++ js/data.js
@@ -7,6 +7,11 @@ var data = {
   nextEntryId: 1
 };
 
+const previousDataJSON = localStorage.getItem('localData');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
 window.addEventListener('beforeunload', function (event) {
   const dataJSON = JSON.stringify(data);
   localStorage.setItem('localData', dataJSON);
